2635ed49904bd65eadf23e4e8b6fea00f10636f7
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-12-22 23:44:08 -0700

    Fix linting errors

--- client/src/data/mockData/mockLocalStorage.js
+++ client/src/data/mockData/mockLocalStorage.js
@@ -1,5 +1,5 @@
 export default {
-  getItem: key => 'abcdefghijk987654321',
+  getItem: () => 'abcdefghijk987654321',
   removeItem: key => delete localStorage.Storage[key],
   Storage: {
     'spirit-token-987': 'abcdefghijk987654321'
